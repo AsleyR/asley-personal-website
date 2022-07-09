d3f6a76af576b1d48762e1f162dbadf9bc46dee5
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-07-09 00:30:02 -0600

    Start working on portfolio section

---
+++ src/views/Portfolio.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Portofolio = () => {
+  return (
+    <div className='portfolio-container'>
+      <div className='area-1'>
+        <div className='section-tag-container'>
+          <p className='feature-projects-tag'>FEATURE PROJECTS</p>
+        </div>
+        <div className='project-about'>
+          <h1>Data Visualizer</h1>
+          <p>
+            A simple python program that visualizes data
+            from a.csv file. It uses the library PySimpleGui
+            for its GUI.
+          </p>
+        </div>
+      </div>
+      <div className='area-2'>
+        <h5>G</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Portofolio
