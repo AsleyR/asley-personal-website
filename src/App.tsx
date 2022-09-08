1d12f98211f87de3249cdef4fc79150f5f828bd9
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-08 13:24:23 -0600

    Complete basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <h1 className='text-4xl'>Hello World</h1>
+      <h2 id='work' className='mt-[50rem]'>Work</h2>
     </div>
   )
 }
