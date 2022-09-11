0de4cbd87d370f5831c7bc6af5a421c521fb60ec
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-10 19:14:53 -0600

    Add footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='grid gap-1 text-center text-sm bg-gray-100 py-12 md:py-3'>
+      <p className=''>This site was hand-crafted, with ❤️.</p>
+      <p className=''>Copyright © Asley Robleto. All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
