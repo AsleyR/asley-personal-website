7bb3dbe4033a1f7aa549d5b49369e1d9824fc5ec
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-11 10:56:15 -0600

    Small change of styles

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div className='py-[6rem]' id='contact'>
         <SectionTag text='CONTACT ME'/>
         <div className='grid grid-cols-auto gap-5 pr-0 lg:pr-[25rem] mt-16 transition-all'>
-            <h1 className='text-2xl md:text-4xl font-bold transition-all'>
+            <h1 className='text-2xl md:text-4xl font-bold hover:scale-[1.02] origin-bottom-left transition-all'>
                 <a className='hover:text-gray-700 duration-50' href="mailto:asleyrobleto@gmail.com">asleyrobleto@gmail.com</a>
             </h1>
             <p className='text-lg md:text-xl leading-9 transition-all'>
@@ -14,10 +14,10 @@ const Contact = () => {
             </p>
             <div>
                 <ul className='flex w-full gap-3 transition-all'>
-                    <a className='w-9' href="https://github.com/AsleyR" target={'_blank'}>
+                    <a className='w-9 hover:scale-105 transition-all' href="https://github.com/AsleyR" target={'_blank'}>
                         <img className='object-cove max-w-full h-auto' src='./images/socials/github.svg'></img>
                     </a>
-                    <a className='w-9' href="https://www.linkedin.com/in/asleyr/" target={'_blank'}>
+                    <a className='w-9 hover:scale-105 transition-all' href="https://www.linkedin.com/in/asleyr/" target={'_blank'}>
                         <img className='object-cover max-w-full h-auto' src='./images/socials/linkedin.svg'></img>
                     </a>
                  </ul>
