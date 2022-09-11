822c3cf992b18934743b1925f6e2db35522d6d2a
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-10 19:17:42 -0600

    Add Work component

---
+++ src/components/home/Work.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import DataVProject from '../projects/DataVProject'
+import TrailerParadise from '../projects/TrailerParadise'
+import Youter from '../projects/Youter'
+import SectionTag from '../SectionTag'
+
+const Work = () => {
+  return (
+    <div className='pt-[3rem] pb-[6rem]' id='work'>
+        <SectionTag text='FEATURE PROJECTS'/>
+        <div className='grid gap-[10rem] mt-16'>
+            <DataVProject/>
+            <Youter/>
+            <TrailerParadise/>
+        </div>
+    </div>
+  )
+}
+
+export default Work
