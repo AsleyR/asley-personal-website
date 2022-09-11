8d05609e4d00832397ed05d8756209f55ddf8e34
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-11 10:39:38 -0600

    Refactor nav links, add '_blank' target to nav links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,8 +15,8 @@ const Header = () => {
             <ul className={`absolute inset-x-0 md:hidden ${openMenu? 'top-[0]':'-top-[30rem]'} px-[1rem] grid bg-white border-y border-gray-300 drop-shadow duration-500 transition-all`}>
                 <a className='hover:bg-gray-100 active:bg-gray-200 border-b border-gray-300 px-[1rem] py-4 cursor-pointer' href='/#work' onClick={() => {changePageTitle('My work - Asley Robleto'); setOpenMenu(false)}}>Work</a>
                 <a className='hover:bg-gray-100 active:bg-gray-200 border-b border-gray-300 px-[1rem] py-4 cursor-pointer' href="/#about" onClick={() => {changePageTitle('About me - Asley Robleto'); setOpenMenu(false)}}>About</a>
-                <a className='hover:bg-gray-100 active:bg-gray-200 border-b border-gray-300 px-[1rem] py-4 cursor-pointer' href="/resume-asleyrobleto.pdf" onClick={() => {changePageTitle('Asley Robleto'); setOpenMenu(false)}}>Resume</a>
-                <a className='hover:bg-gray-100 active:bg-gray-200 px-[1rem] py-4 cursor-pointer' href="/#contact" onClick={() => {changePageTitle('Contact me - Asley Robleto'); setOpenMenu(false)}}>Contact</a>
+                <a className='hover:bg-gray-100 active:bg-gray-200 border-b border-gray-300 px-[1rem] py-4 cursor-pointer' href="/#contact" onClick={() => {changePageTitle('Contact me - Asley Robleto'); setOpenMenu(false)}}>Contact</a>
+                <a className='hover:bg-gray-100 active:bg-gray-200 px-[1rem] py-4 cursor-pointer' href="/resume-asleyrobleto.pdf" target={'_blank'} onClick={() => {changePageTitle('Asley Robleto'); setOpenMenu(false)}}>Resume</a>
             </ul>
         </div>
     )
@@ -34,8 +34,8 @@ const Header = () => {
         <ul className='hidden md:inline-flex gap-16'>
             <li><a className='hover:text-gray-500 duration-150' href="/#work" onClick={() => changePageTitle('My work - Asley Robleto')}>Work</a></li>
             <li><a className='hover:text-gray-500 duration-150' href="/#about" onClick={() => changePageTitle('About me - Asley Robleto')}>About</a></li>
-            <li><a className='hover:text-gray-500 duration-150' href="/resume-asleyrobleto.pdf" target={'_blank'} onClick={() => changePageTitle('Asley Robleto')}>Resume</a></li>
             <li><a className='hover:text-gray-500 duration-150' href="/#contact" onClick={() => changePageTitle('Contact me - Asley Robleto')}>Contact</a></li>
+            <li><a className='hover:text-gray-500 duration-150' href="/resume-asleyrobleto.pdf" target={'_blank'} onClick={() => changePageTitle('Asley Robleto')}>Resume</a></li>
         </ul>
         {openMenu ?
         <FaTimes className='block md:hidden cursor-pointer text-3xl' onClick={() => setOpenMenu(!openMenu)}/>
