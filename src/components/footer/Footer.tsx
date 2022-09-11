6e7ad4af5b62cb04c0c5498bfeacc1b3b2462d05
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-11 10:54:41 -0600

    Change bg to 'gray-bg'

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='grid gap-1 text-center text-sm bg-gray-100 py-12 md:py-3'>
+    <div className='grid gap-1 text-center text-sm bg-gray-bg py-12 md:py-3'>
       <p className=''>This site was hand-crafted, with ❤️.</p>
       <p className=''>Copyright © Asley Robleto. All Rights Reserved</p>
     </div>
