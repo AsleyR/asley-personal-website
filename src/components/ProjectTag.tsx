0a1b598dfe2db5d7ef1191d4a6628aa48ef7e94d
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-13 19:22:53 -0600

    Add 'cursor-default' property

--- src/components/ProjectTag.tsx
+++ src/components/ProjectTag.tsx
@@ -7,7 +7,7 @@ interface IProps {
 const ProjectTag = ({text}: IProps) => {
   return (
     <div className='bg-gray-100 rounded-full px-4'>
-      <p className='text-[#6c6c6c]'>{text}</p>
+      <p className='cursor-default text-[#6c6c6c]'>{text}</p>
     </div>
   )
 }
