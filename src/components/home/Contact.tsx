3d8304f4106c5785f3531f5e722c3c7b28515273
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-09-13 22:03:49 -0600

    Add Fade component

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import SectionTag from '../SectionTag'
 import Socials from '../Socials'
+import { Fade } from 'react-awesome-reveal'
 
 const Contact = () => {
   return (
     <article className='py-[6rem]' id='contact'>
         <SectionTag text='CONTACT ME'/>
-        <section className='grid grid-cols-auto gap-5 pr-0 lg:pr-[25rem] mt-16 transition-all'>
-            <address className='text-2xl md:text-4xl font-bold hover:scale-[1.02] origin-bottom-left transition-all'>
-                <a className='not-italic hover:text-gray-700 duration-50' href="mailto:asleyrobleto@gmail.com">asleyrobleto@gmail.com</a>
-            </address>
-            <p className='text-lg md:text-xl leading-9 transition-all'>
-                I'm always down to collaborate when I have the time. I'm dream of becoming a great developer and one day disrupt the tech ecosystem with a cool project.
-            </p>
-            <Socials/>
-        </section>
+        <Fade direction='up' triggerOnce={true}>
+          <section className='grid grid-cols-auto gap-5 pr-0 lg:pr-[25rem] mt-16 transition-all'>
+              <address className='text-2xl md:text-4xl font-bold hover:scale-[1.02] origin-bottom-left transition-all'>
+                  <a className='not-italic hover:text-gray-700 duration-50' href="mailto:asleyrobleto@gmail.com">asleyrobleto@gmail.com</a>
+              </address>
+              <p className='text-lg md:text-xl leading-9 transition-all'>
+                  I'm always down to collaborate when I have the time. I'm dream of becoming a great developer and one day disrupt the tech ecosystem with a cool project.
+              </p>
+              <Socials/>
+          </section>
+        </Fade>
     </article>
   )
 }
