219d8f7f2971e3bd717f4a78326d95c7238b72e0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-03 14:52:43 -0600

    Rephrase about section

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -11,10 +11,10 @@ const About = () => {
           <section className='grid gap-5'>
             <h1 className='font-source-serif text-4xl font-bold'>Python and full stack developer from Montreal, Canada.</h1>
             <p className='text-lg leading-9'>
-            I'm a solution oriented full stack developer. I'm fascinated by all-things regarding the world of tech, and I love the idea of creating products that can help someone's life for the better. If I'm not learning some new tech, or working on a personal project, I'm chilling with a cup of coffee by my side.
+            I'm a solution oriented full stack developer. I'm fascinated by all-things regarding the world of tech, and I enjoy working in web development because of the satisfaction that I get by overcoming challenges and designing pretty websites. I'm motivated by the idea of working on software that may help someone life for the better. If I'm not learning some new tech, or working on a personal project, I'm chilling with a cup of coffee by my side.
             </p>
             <p className='text-lg leading-9'>
-              I work mostly with React and Express to make web apps, but for my other projects, I usually just work with Python to create small desktop programs using the PySimpleGui library if the need for a Gui arises. 
+              I work mostly with React and Express to make web apps, but for my other projects, I usually just work with Python to create small desktop programs using various tools and libraries depending on what the project requires.
             </p>
           </section>
           <div className='flex justify-center w-0 lg:w-[20rem]'>
