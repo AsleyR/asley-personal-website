1de957393e1140e59bc31e1666088a2b05f491eb
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-10 15:16:52 -0600

    Make project tags change to darker color on hover

--- src/components/ProjectTag.tsx
+++ src/components/ProjectTag.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 const ProjectTag = ({text}: IProps) => {
   return (
-    <div className='bg-gray-100 rounded-full px-4'>
+    <div className='bg-gray-100 rounded-full px-4 hover:bg-gray-200 hover:transition-all'>
       <p className='cursor-default text-[#6c6c6c]'>{text}</p>
     </div>
   )
