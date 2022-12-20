1cb5eaf4717474097c1936ac26b02393e56b534d
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-19 18:03:34 -0600

    Update footer to show new license.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='grid gap-1 text-center text-sm bg-gray-bg py-12 md:py-3'>
       <p className=''>This site was hand-crafted, with ❤️.</p>
-      <p className=''>Copyright © Asley Robleto. All Rights Reserved</p>
+      <p className=''>Licensed under <a className="underline" href="https://github.com/AsleyR/asley-personal-website/blob/main/LICENSE" target={"_blank"}>GPL-3.0-or-later</a>. All Rights Reserved.</p>
     </div>
   )
 }
