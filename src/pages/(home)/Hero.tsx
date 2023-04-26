9a7b448796a87d44f5e2c5b383f3be728531ac33
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-25 23:53:34 -0600

    Move Hero.tsx

--- src/pages/(home)/Hero.tsx
+++ src/pages/(home)/Hero.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import BtnUnderline from '../BtnUnderline'
 import { Fade } from 'react-awesome-reveal'
+import BtnUnderline from '../../components/BtnUnderline'
 
 const Hero = () => {
   return (
     <article className='bg-gray-bg md:bg-transparent align-middle grid grid-cols-auto lg:grid-cols-2 pb-[7.2rem] py-[3rem] md:py-[3rem] md:pt-[7rem] md:pb-[11rem] px-[2rem] sm:px-[4rem] xl:px-[11rem]'>
-       <Fade direction='down' triggerOnce={true}>
+      <Fade direction='down' triggerOnce={true}>
         <section className='relative grid gap-5 mlg:gap-6'>
-            <h1 className='font-source-serif font-bold text-6xl sm:text-7xl lg:text-6xl'>Hello there 👋</h1>
-            <p className='leading-9 text-xl lg:text-2xl lg:leading-[40px] pb-[2rem]'>I'm Asley, a python and full stack developer based in Montreal, Canada. I love learning new stuff and creating pretty websites.</p>
-            <BtnUnderline text='Check out my work' link='work'/>
+          <h1 className='font-source-serif font-bold text-6xl sm:text-7xl lg:text-6xl'>Hello there 👋</h1>
+          <p className='leading-9 text-xl lg:text-2xl lg:leading-[40px] pb-[2rem]'>I'm Asley, a python and full stack developer based in Montréal, Canada. I love learning new stuff and creating pretty websites.</p>
+          <BtnUnderline text='Check out my work' link='work' />
         </section>
       </Fade>
       <Fade direction='down' triggerOnce={true}>
