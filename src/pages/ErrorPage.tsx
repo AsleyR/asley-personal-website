2bc512aade77cd7fbd5f51b0274d8d9816984ccb
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-25 23:55:41 -0600

    Delete unused import

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
@@ -8,13 +7,13 @@ const ErrorPage = () => {
         <h1 className='text-center text-8xl font-bold'>404</h1>
         <p className='text-center text-lg pt-4'>Oops! We Couldn't Find The Page You Were Looking For.</p>
         <p className='text-center pt-1'>Perhaps, return to the &#20;
-        <span className='inline-flex'>
-          <Link to={'/'} className="underline md:no-underline group text-blue-500 transition duration-300">
-            homepage
-            <span className="md:block max-w-0 group-hover:max-w-full transition-all duration-300 h-[1px] bg-blue-500"></span>
-          </Link>
-        </span>
-        ?
+          <span className='inline-flex'>
+            <Link to={'/'} className="underline md:no-underline group text-blue-500 transition duration-300">
+              homepage
+              <span className="md:block max-w-0 group-hover:max-w-full transition-all duration-300 h-[1px] bg-blue-500"></span>
+            </Link>
+          </span>
+          ?
         </p>
       </article>
     </main>
