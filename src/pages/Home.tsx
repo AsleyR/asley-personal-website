d7c469d56c4b5f008a7958102d7c442723bf6fa9
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-25 23:56:03 -0600

    Delete unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import Footer from '../components/footer/Footer'
-import About from '../components/home/About'
-import Contact from '../components/home/Contact'
-import Hero from '../components/home/Hero'
-import Work from '../components/home/Work'
+import About from './(home)/About'
+import Hero from './(home)/Hero'
+import Work from './(home)/Work'
+import Contact from './(home)/Contact'
 
 const Home = () => {
   return (
     <main>
-      <Hero/>
+      <Hero />
       <div className='px-[2rem] sm:px-[4rem] xl:px-[11rem] transition-all'>
-        <Work/>
-        <About/>
-        <Contact/>
+        <Work />
+        <About />
+        <Contact />
       </div>
-      <Footer/>
+      <Footer />
     </main>
   )
 }
