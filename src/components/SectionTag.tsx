8e178996640a85671882fc888a2ae7a11029563e
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-25 23:52:21 -0600

    Refactor SectionTag.tsx

--- src/components/SectionTag.tsx
+++ src/components/SectionTag.tsx
@@ -1,14 +1,8 @@
-import React from 'react'
-
-interface IProps {
-    text: string;
-}
-
-const SectionTag = ({text}: IProps) => {
+const SectionTag = ({ title }: { title: string }) => {
   return (
     <h1 className='flex items-center gap-2'>
       <span className='w-[25px] md:w-[50px] h-[1px] bg-black inline-block'></span>
-      <p className='text-sm md:text-base'>{text}</p>
+      <p className='text-sm md:text-base'>{title}</p>
     </h1>
   )
 }
