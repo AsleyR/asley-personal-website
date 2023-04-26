bc7ec02d7784d7caef2affe3f1458cfbcc009674
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-25 23:55:24 -0600

    Add ProjectContainerProps.ts

---
+++ src/lib/types/ProjectContainerProps.ts
@@ -0,0 +1,8 @@
+export interface ProjectContainerProps {
+    title: string;
+    description: string;
+    imagePath: string;
+    repoLink: string;
+    link?: string;
+    projectTags?: string[] | undefined
+}
