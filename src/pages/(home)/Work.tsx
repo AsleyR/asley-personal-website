3dd9de8e3df634426bd254d5992e2ff6bde2af22
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-25 23:53:18 -0600

    Move Work.tsx

---
+++ src/pages/(home)/Work.tsx
@@ -0,0 +1,13 @@
+import SectionTag from "../../components/SectionTag"
+import ProjectSection from "../../components/tag/ProjectSection"
+
+const Work = () => {
+  return (
+    <article className='py-[6rem]' id='work'>
+      <SectionTag title='FEATURE PROJECTS' />
+      <ProjectSection />
+    </article>
+  )
+}
+
+export default Work
