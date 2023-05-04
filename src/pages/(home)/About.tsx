02991350c79f7784007c45674a42cb97cf711f62
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-03 22:49:37 -0600

    Update About.tsx

--- src/pages/(home)/About.tsx
+++ src/pages/(home)/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
               I'm a solution oriented full stack developer. I'm fascinated by all-things regarding the world of tech, and I enjoy working in web development because of the satisfaction that I get by overcoming challenges and designing pretty websites. I'm motivated by the idea of working on software that may help someone life for the better. If I'm not learning some new tech, or working on a personal project, I'm chilling with a cup of coffee by my side.
             </p>
             <p className='text-lg leading-9'>
-              I work mostly with React and Express to make web apps, but for my other projects, I usually just work with Python to create small desktop programs using various tools and libraries depending on what the project requires.
+              I work mostly with Next.js, Express and MongoDB to make web apps, but for my other projects, I usually just work with Python to create small desktop programs using various tools and libraries depending on what the project requires.
             </p>
           </section>
           <div className='flex justify-center w-0 lg:w-[20rem]'>
