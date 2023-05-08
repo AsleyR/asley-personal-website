7a27d6f562dc1044cafe1aecc9c24ba57cfe86a0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-08 00:06:59 -0600

    Update imports & Fix lint issue

--- app/(components)/hero/HeroText.tsx
+++ app/(components)/hero/HeroText.tsx
@@ -1,11 +1,11 @@
-import { sourceSerifPro } from "@/app/layout"
+import { sourceSerifPro } from "@/app/(libs)/fonts/SourceSerifPro"
 import ButtonComponent from "../ButtonComponent"
 
 const HeroText = () => {
     return (
         <div className="flex flex-col gap-5">
             <h1 className={`font-bold text-6xl ${sourceSerifPro.className}`}>Hello there 👋</h1>
-            <p className="font-base text-xl md:text-2xl tracking-wider">I'm Asley, a python and full stack developer based in Montréal, Canada. I love learning new stuff and creating pretty websites.</p>
+            <p className="font-base text-xl md:text-2xl tracking-wider">{`I'm Asley, a python and full stack developer based in Montréal, Canada. I love learning new stuff and creating pretty websites.`}</p>
             <div className="flex">
                 <ButtonComponent text="Check out my work" className="text-lg" />
             </div>
--- app/(components)/sections/AboutSection.tsx
+++ app/(components)/sections/AboutSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import SectionTag from "../SectionTag"
-import { sourceSerifPro } from "../../layout"
+import { sourceSerifPro } from "@/app/(libs)/fonts/SourceSerifPro"
+
 
 const About = () => {
     return (
@@ -8,10 +9,10 @@ const About = () => {
             <div className="flex flex-col gap-5 text-lg">
                 <h1 className={`${sourceSerifPro.className} font-bold text-3xl md:text-4xl`}>Python and full stack developer from Montréal, Canada.</h1>
                 <p className="leading-9">
-                    I'm a solution oriented full stack developer. I'm fascinated by all-things regarding the world of tech, and I enjoy working in web development because of the satisfaction that I get by overcoming challenges and designing pretty websites. I'm motivated by the idea of working on software that may help someone life for the better. If I'm not learning some new tech, or working on a personal project, I'm chilling with a cup of coffee by my side.
+                    {`I'm a solution oriented full stack developer. I'm fascinated by all-things regarding the world of tech, and I enjoy working in web development because of the satisfaction that I get by overcoming challenges and designing pretty websites. I'm motivated by the idea of working on software that may help someone life for the better. If I'm not learning some new tech, or working on a personal project, I'm chilling with a cup of coffee by my side.`}
                 </p>
                 <p className="leading-9">
-                    I work mostly with Next.js, Express and MongoDB to make web apps, but for my other projects, I usually just work with Python to create small desktop programs using various tools and libraries depending on what the project requires.
+                    {`I work mostly with Next.js, Express and MongoDB to make web apps, but for my other projects, I usually just work with Python to create small desktop programs using various tools and libraries depending on what the project requires.`}
                 </p>
             </div>
             <div className="hidden lg:flex justify-center w-[23rem] h-full">
