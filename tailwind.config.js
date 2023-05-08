6f14ea753870a14349843f9949a7758ce4876c28
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-07 23:43:58 -0600

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "bggray": "#f2f2f2"
       },
+      padding: {
+        "normalx": "10rem",
+        "mobilex": "2rem"
+      },
+      margin: {
+        "normalx": "10rem",
+        "mobilex": "2rem"
+      }
     },
   },
   plugins: [],
