9240663ec2d7284b8a4c116ee0b062e8c641dae5
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-07 23:44:26 -0600

    Add index.types.ts

---
+++ app/(types)/index.types.ts
@@ -0,0 +1,38 @@
+export interface ComponentProps {
+    text?: string;
+    className?: string;
+}
+
+export interface ContainerProps extends ComponentProps {
+    children: React.ReactNode
+}
+
+export interface NavbarComponentProps extends ComponentProps {
+}
+
+export interface NavLinkComponentProps extends ComponentProps {
+    link: string;
+    onClick?: (arg: any) => void;
+}
+
+export interface IconComponentProps extends ComponentProps {
+}
+
+export interface ButtonComponentProps extends ComponentProps {
+    text: string
+}
+
+export interface SectionTagComponentProps extends ComponentProps {
+    id?: string;
+    text: string;
+}
+
+export interface PortfolioProject {
+    title: string;
+    role?: string;
+    description: string;
+    tags: string[];
+    url?: string;
+    repoUrl: string;
+    image: string;
+}
