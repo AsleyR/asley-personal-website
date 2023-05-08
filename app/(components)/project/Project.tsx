daf5c61d9d0eb30040aea95734dd2a6ff8b8f06d
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-07 23:46:47 -0600

    Add Project.tsx

---
+++ app/(components)/project/Project.tsx
@@ -0,0 +1,25 @@
+import { PortfolioProject } from "@/app/(types)/index.types"
+import Image from "next/image"
+import Link from "next/link"
+import ProjectContent from "./ProjectContent"
+
+const Project = ({ project, direction }: { project: PortfolioProject, direction?: "left" | "right" }) => {
+    if (!direction) {
+        direction = "left"
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
+            <Link className={`${direction === 'left' ? "order-first" : "order-first lg:order-last"} h-min shadow-box-xl hover:scale-105 transition-all duration-300 cursor-pointer`}
+                href={project.url || project.repoUrl} target={'_blank'} >
+                <Image
+                    className='w-full h-full rounded-lg border'
+                    width={500} height={500}
+                    src={project.image} alt={`${project.title} project photo`} />
+            </Link>
+            <ProjectContent project={project} />
+        </div>
+    )
+}
+
+export default Project
