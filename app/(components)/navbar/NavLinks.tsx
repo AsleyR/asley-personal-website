3de1e5b1c3d1b0a0133263e4fc210fcae663b7c5
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-07 23:46:43 -0600

    Add NavLinks.tsx

---
+++ app/(components)/navbar/NavLinks.tsx
@@ -0,0 +1,16 @@
+import { NavLinkComponentProps, NavbarComponentProps } from "@/app/(types)/index.types"
+import NavLink from "./NavLink"
+
+const NavLinks = ({ className }: NavbarComponentProps) => {
+    return (
+        <div className={`flex justify-end items-center align-middle ${className}`}>
+            <ul className="flex gap-16 tracking-wide">
+                <NavLink link="#work" text="Work" />
+                <NavLink link="/#about" text="About" />
+                <NavLink link="/#contact" text="Contact" />
+            </ul>
+        </div>
+    )
+}
+
+export default NavLinks
