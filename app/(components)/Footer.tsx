f6ad737e9e7287ceb70faf4c7767ac82ac017a54
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-08 00:16:19 -0600

    Add mobile padding

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Footer = () => {
     return (
-        <footer className="flex flex-col justify-center py-5 text-sm text-center bg-bggray">
+        <footer className="flex flex-col justify-center px-mobilex py-5 text-sm text-center bg-bggray">
             <p>This site was hand-crafted, with ❤️.</p>
             <p>Licensed under <Link className="underline hover:text-gray-500" href={'https://github.com/AsleyR/asley-personal-website/blob/main/LICENSE.md'} target={'_blank'}>GPL-3.0-or-later</Link>. All Rights Reserved.</p>
         </footer>
