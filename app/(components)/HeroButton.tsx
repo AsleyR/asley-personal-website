835db9cf1753de46c886535e34de66a3277f3031
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-08 00:16:12 -0600

    Rename component

--- app/(components)/HeroButton.tsx
+++ app/(components)/HeroButton.tsx
@@ -1,12 +1,14 @@
-import { ButtonComponentProps } from "../(types)/index.types"
+import { ButtonLinkComponentProps } from "../(types)/index.types"
 
-const ButtonComponent = ({ text, className }: ButtonComponentProps) => {
+const HeroButton = ({ text, className, link }: ButtonLinkComponentProps) => {
     return (
         <button className={`${className} group hover:scale-110 md:origin-bottom-left transition-all font-bold duration-300`}>
-            {text}
+            <a href={link}>
+                {text}
+            </a>
             <span className="md:block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
         </button>
     )
 }
 
-export default ButtonComponent
+export default HeroButton
