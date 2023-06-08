6c1a788e309ff21be47c6955b0623aa3e20d4c18
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-08 18:39:52 -0400

    Move parseUpdateRequestBody to its own function

--- app/api/projects/update/route.ts
+++ app/api/projects/update/route.ts
@@ -1,37 +1,5 @@
+import parseUpdateRequestBody from "@/app/(libs)/api/parseUpdateRequest"
 import { prisma } from "@/app/(libs)/client"
-
-function parseUpdateRequestBody(res: any) {
-    if (!res) {
-        return new Response(JSON.stringify({
-            error: "Bad Request"
-        }), {
-            status: 400
-        })
-    }
-    let cause: any = []
-
-    if (!res.projectId || !res.key) {
-
-        !res.projectId ? cause.push('projectId') : null
-        !res.key ? cause.push('key') : null
-
-        if (res.key !== process.env.API_KEY) {
-            return new Response(JSON.stringify({
-                error: "Not Authorized.",
-                cause: "This request is not authorized to perform an update action."
-            }), {
-                status: 401
-            })
-        }
-
-        return new Response(JSON.stringify({
-            error: "Bad request.",
-            cause: `Missing ${cause.map((str: string) => `'${str}'`)} parameters`
-        }), {
-            status: 400
-        })
-    }
-}
 
 export async function POST(request: Request) {
     const res = await request.json().catch(err => null)
@@ -99,8 +67,6 @@ export async function POST(request: Request) {
         },
         "data": reqObject
     }).catch(err => err)
-
-    // const editedProject = reqObject
 
     return new Response(JSON.stringify(editedProject), {
         status: 200
