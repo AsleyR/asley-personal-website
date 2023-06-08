67cbb1e72bf57335f54daefa1933d011935f7afd
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-08 18:39:17 -0400

    Move parseCreateRequestBody to its own function

--- app/api/projects/create/route.ts
+++ app/api/projects/create/route.ts
@@ -1,119 +1,5 @@
+import parseCreateRequestBody from "@/app/(libs)/api/parseCreateRequest"
 import { prisma } from "@/app/(libs)/client"
-
-type CreateRequestBody = {
-    key: string;
-    data: any | any[]
-}
-
-/**
- * Parses request body.
- * @param res
- * @returns `Response` or `Undefined`
- */
-function parseCreateRequestBody(res: CreateRequestBody): Response | undefined {
-    if (!res) {
-        return new Response(JSON.stringify({
-            error: "Bad Request"
-        }), {
-            status: 400
-        })
-    }
-
-
-    if (!Array.isArray(res.data)) {
-        let cause: any = []
-
-        // res.tags is not checked because it's an optional value
-        if (!res.key || !res.data.title || !res.data.role || !res.data.description || !res.data.image || !res.data.repoUrl || !res.data.url) {
-
-            !res.key ? cause.push('key') : null
-            console.log(res)
-            !res.data.title ? cause.push("title") : null
-            !res.data.role ? cause.push('role') : null
-            !res.data.description ? cause.push('description') : null
-            !res.data.image ? cause.push('image') : null
-            !res.data.repoUrl ? cause.push('repoUrl') : null
-            !res.data.url ? cause.push('url') : null
-
-            if (res.key !== process.env.API_KEY) {
-                return new Response(JSON.stringify({
-                    error: "Not Authorized.",
-                    cause: "This request is not authorized to perform a create action."
-                }), {
-                    status: 401
-                })
-            }
-
-            return new Response(JSON.stringify({
-                error: "Bad request.",
-                cause: `Missing ${cause.map((str: string) => `'${str}'`)} parameters`
-            }), {
-                status: 400
-            })
-        }
-    } else {
-        let parsedCause: any = []
-
-        if (!res.key) {
-            return new Response(JSON.stringify({
-                error: "Bad request.",
-                cause: "Missing 'key' parameters"
-            }), {
-                status: 400
-            })
-        }
-
-        if (res.key !== process.env.API_KEY) {
-            return new Response(JSON.stringify({
-                error: "Not Authorized.",
-                cause: "This request is not authorized to perform a create action."
-            }), {
-                status: 401
-            })
-        }
-
-        res.data.map((res: any, index: number) => {
-            let cause: any = []
-
-            // res.tags is not checked because it's an optional value
-            if (!res.title || !res.role || !res.description || !res.image || !res.repoUrl) {
-
-                !res.title ? cause.push("title") : null
-                !res.role ? cause.push('role') : null
-                !res.description ? cause.push('description') : null
-                !res.image ? cause.push('image') : null
-                !res.repoUrl ? cause.push('repoUrl') : null
-                // !res.url ? cause.push('url') : null
-
-                const result = {
-                    position: index,
-                    cause: `Missing ${cause.map((str: string) => `'${str}'`)} parameters`,
-                }
-                cause = []
-
-                parsedCause.push(result)
-            }
-
-        })
-
-        // Map through the array and checks if each value equals to undefined,
-        // and returns result into new array.
-        // Then, it checks if the values of the new array are equal to false
-        const areErrorsPresent = parsedCause.map((value: any) => {
-            return value !== undefined
-        }).every((value: any) => value === false)
-
-        if (parsedCause.length !== 0 && !areErrorsPresent) {
-            return new Response(JSON.stringify({
-                error: "Bad request.",
-                cause: parsedCause
-            }), {
-                status: 400
-            })
-        }
-
-    }
-}
 
 export async function POST(request: Request) {
     const { searchParams } = new URL(request.url)
@@ -234,8 +120,6 @@ export async function POST(request: Request) {
                 status: 400
             })
     }
-
-    // newProject = reqObject
 
     return new Response(JSON.stringify(newProject), {
         status: 200
