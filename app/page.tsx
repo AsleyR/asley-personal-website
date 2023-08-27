c1befdc7395ad63dc2cdd6a9ce255061fe5570bb
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-08-27 17:40:27 -0400

    Fix broken fetches

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,15 @@ import ContactSection from "./(components)/sections/ContactSection"
 import fetchProjects from "./(libs)/fetchProjects"
 import WorkSection from "./(components)/sections/WorkSection"
 import fetchWorks from "./(libs)/fetchWorks"
+import getAllProjects from "./(actions)/projects/getAllProjects"
+import getAllWorks from "./(actions)/works/getAllWorks"
 
 export default async function Home() {
-  const projects = await fetchProjects()
-  const works = await fetchWorks()
+  // const projects = await fetchProjects()
+  // const works = await fetchWorks()
+
+  const projects = await getAllProjects()
+  const works = await getAllWorks()
 
   return (
     <>
