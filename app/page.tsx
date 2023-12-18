6112bbae2bbdbd342de6e96d1fb9e1ef43695895
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-18 13:09:52 -0500

    clean unused code

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,11 @@ import Footer from "./(components)/Footer"
 import HeroSection from "./(components)/sections/HeroSection"
 import PortfolioSection from "./(components)/sections/PortfolioSection"
 import ContactSection from "./(components)/sections/ContactSection"
-import fetchProjects from "./(libs)/fetchProjects"
 import WorkSection from "./(components)/sections/WorkSection"
-import fetchWorks from "./(libs)/fetchWorks"
 import getAllProjects from "./(actions)/projects/getAllProjects"
 import getAllWorks from "./(actions)/works/getAllWorks"
 
 export default async function Home() {
-  // const projects = await fetchProjects()
-  // const works = await fetchWorks()
-
   const projects = await getAllProjects()
   const works = await getAllWorks()
 
