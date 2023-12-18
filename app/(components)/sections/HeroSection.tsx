3ef0141c9b7f6fff2cbe37828c0567591d8e36e9
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-18 13:09:26 -0500

    Add polka dots

--- app/(components)/sections/HeroSection.tsx
+++ app/(components)/sections/HeroSection.tsx
@@ -3,7 +3,8 @@ import HeroText from "../hero/HeroText"
 
 const HeroSection = () => {
     return (
-        <section className="bg-[#f2f2f2] md:bg-inherit grid grid-cols-1 lg:grid-cols-2 items-center align-middle py-[8rem] px-mobilex lg:px-normalx">
+        <section className="relative z-10 bg-[#f2f2f2] md:bg-inherit grid grid-cols-1 lg:grid-cols-2 items-center align-middle py-[8rem] px-mobilex lg:px-normalx">
+            <div className="absolute inset-0 -z-10 polka-dots"></div>
             <div className="z-20 w-full h-full">
                 <HeroText />
             </div>
