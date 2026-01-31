858f0d1574ea0ed894a9918badcb879083a5d164
AsleyR <asleyrobleto@gmail.com>
2026-01-31 16:47:37 -0500

    Change hero text sizes

--- app/(components)/hero/HeroText.tsx
+++ app/(components)/hero/HeroText.tsx
@@ -4,10 +4,16 @@ import HeroButton from "../HeroButton"
 const HeroText = () => {
     return (
         <div className="flex flex-col gap-5">
-            <h1 className={`font-bold text-6xl ${sourceSerifPro.className}`}>Hello there 👋</h1>
-            <p className="font-base font-medium text-xl md:text-2xl tracking-wide">
-                {`I'm Asley, a python and full stack developer based in Montréal, Canada. I love learning new stuff and creating pretty websites.`}
-            </p>
+            <h1 className={`font-bold text-5xl lg:text-6xl ${sourceSerifPro.className}`}>Hello there 👋</h1>
+            <div className="flex flex-col gap-3 font-base font-medium 
+            text-lg md:text-2xl tracking-wide">
+                <p className="">
+                    {`I'm Asley, a python and full stack developer based in Montréal, Canada.`}
+                </p>
+                <p>
+                    {`I love learning new stuff and creating pretty websites.`}
+                </p>
+            </div>
             <div className="flex">
                 <HeroButton link="/#work" text="Check out my work" className="text-xl md:text-lg" />
             </div>
