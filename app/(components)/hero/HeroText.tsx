f30dbe29c4895a50e852ca3f3e588e265ef81b49
AsleyR <asleyrobleto@gmail.com>
2026-01-31 15:34:57 -0500

    Small style change in text

--- app/(components)/hero/HeroText.tsx
+++ app/(components)/hero/HeroText.tsx
@@ -5,7 +5,9 @@ const HeroText = () => {
     return (
         <div className="flex flex-col gap-5">
             <h1 className={`font-bold text-6xl ${sourceSerifPro.className}`}>Hello there 👋</h1>
-            <p className="font-base text-xl md:text-2xl tracking-wider">{`I'm Asley, a python and full stack developer based in Montréal, Canada. I love learning new stuff and creating pretty websites.`}</p>
+            <p className="font-base font-medium text-xl md:text-2xl tracking-wide">
+                {`I'm Asley, a python and full stack developer based in Montréal, Canada. I love learning new stuff and creating pretty websites.`}
+            </p>
             <div className="flex">
                 <HeroButton link="/#work" text="Check out my work" className="text-xl md:text-lg" />
             </div>
