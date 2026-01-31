97184f28403453500d1c17641e1e8731ac0190db
AsleyR <asleyrobleto@gmail.com>
2026-01-31 15:35:44 -0500

    Move AboutSection and ContactSection out of the main Container component. Add custom css separators. Add custom svg separator.

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,18 @@ export default async function Home() {
   return (
     <>
       <HeroSection />
-      <div className="md:bg-gray-100 md:border-y border-gray-200 w-full px-mobilex lg:px-normalx">
+      <div className="bg-[#fafaf5] border-y border-gray-200 w-full 
+        px-mobilex lg:px-normalx">
         <WorkSection works={works} />
+      </div>
+      <div className="-mt-5 drop-shadow-sm h-[8rem]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fbfbf2" fillOpacity="1" d="M0,128L120,122.7C240,117,480,107,720,122.7C960,139,1200,181,1320,202.7L1440,224L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
       </div>
       <Container className="h-full w-full">
         <PortfolioSection projects={projects} />
-        <AboutSection />
-        <ContactSection />
       </Container>
+      <AboutSection />
+      <ContactSection />
       <Footer />
     </>
   )
