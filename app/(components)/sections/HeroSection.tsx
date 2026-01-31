2756877f5687c114cfa3e9a686b729647b279201
AsleyR <asleyrobleto@gmail.com>
2026-01-31 16:49:22 -0500

    Moved Hero pic background to an absolute div. Add responsive mobile pic and background.

--- app/(components)/sections/HeroSection.tsx
+++ app/(components)/sections/HeroSection.tsx
@@ -1,17 +1,36 @@
+import Image from "next/image"
 import HeroImage from "../hero/HeroImage"
 import HeroText from "../hero/HeroText"
 
 const HeroSection = () => {
     return (
         <>
-            <section className="relative z-10 bg-[#f2f2f2] md:bg-inherit grid grid-cols-1 
-            lg:grid-cols-2 items-center align-middle py-[8rem] px-mobilex lg:px-normalx
-            hero-pic text-gray-50">
+            <section className="relative z-10 bg-[#2c2718] lg:bg-inherit grid grid-cols-1 
+            lg:grid-cols-2 items-center align-middle gap-5 lg:gap-5
+            py-[4rem] lg:py-[8rem] px-mobilex lg:px-normalx
+            text-gray-50">
+                {/* Regular background */}
                 <div className="absolute inset-0 -z-10 bg-black/40"></div>
-                <div className="z-20 w-full h-full">
+                <div className="hidden lg:block absolute w-full h-full inset-0 -z-20 hero-pic"></div>
+                
+                {/* Mobile Background */}
+                <div className="polka-dots absolute block lg:hidden inset-0 -z-10"></div>
+
+                <div className="relative z-20 w-full h-full rounded-xl p-5 ml-0 lg:-ml-5">
+                    <div className="hidden lg:block absolute inset-0 -z-10 text-glass-effect"></div>
                     <HeroText />
                 </div>
-                {/* <HeroImage /> */}
+                <div className="justify-self-center">
+                    <div className="rounded-xl shadow-2xl">
+                        <Image
+                        className="flex lg:hidden rounded-xl"
+                        src={'/images/asley-pics/asley-1.jpg'}
+                        alt="Asley mobile pic"
+                        width={500}
+                        height={500}
+                        />
+                    </div>
+                </div>
             </section>
             <div className="w-full h-8 bg-[#202020] drop-shadow-md"></div>
         </>
