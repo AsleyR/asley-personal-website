2f016d0607d6497310bf5f75b55aa3f3f3181467
AsleyR <asleyrobleto@gmail.com>
2026-01-31 15:38:57 -0500

    Add fixed background image of Asley. Remove old svg that served from HeroImage component. Updated css style. Remove polka-dots style.

--- app/(components)/sections/HeroSection.tsx
+++ app/(components)/sections/HeroSection.tsx
@@ -3,13 +3,18 @@ import HeroText from "../hero/HeroText"
 
 const HeroSection = () => {
     return (
-        <section className="relative z-10 bg-[#f2f2f2] md:bg-inherit grid grid-cols-1 lg:grid-cols-2 items-center align-middle py-[8rem] px-mobilex lg:px-normalx">
-            <div className="absolute inset-0 -z-10 polka-dots"></div>
-            <div className="z-20 w-full h-full">
-                <HeroText />
-            </div>
-            <HeroImage />
-        </section>
+        <>
+            <section className="relative z-10 bg-[#f2f2f2] md:bg-inherit grid grid-cols-1 
+            lg:grid-cols-2 items-center align-middle py-[8rem] px-mobilex lg:px-normalx
+            hero-pic text-gray-50">
+                <div className="absolute inset-0 -z-10 bg-black/40"></div>
+                <div className="z-20 w-full h-full">
+                    <HeroText />
+                </div>
+                {/* <HeroImage /> */}
+            </section>
+            <div className="w-full h-8 bg-[#202020] drop-shadow-md"></div>
+        </>
     )
 }
 
